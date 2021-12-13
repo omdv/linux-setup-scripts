7049bbd07d9eac2ec4e848a0b9a673a264443e36
Oleg Medvedev <omdv@fastmail.com>
2021-12-12 19:52:33 -0600

    apps, system updates

---
+++ restore.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+rsync -avuP $BACKUP/Calibre ~/
+rsync -avuP $BACKUP/Databases ~/
+rsync -avuP $BACKUP/Desktop ~/
+rsync -avuP $BACKUP/Documents ~/
+rsync -avuP $BACKUP/Downloads ~/
+rsync -avuP $BACKUP/Music ~/
+rsync -avuP $BACKUP/Pictures ~/
+rsync -avuP $BACKUP/Videos ~/
+rsync -avuP $BACKUP/VirtualMachines ~/
